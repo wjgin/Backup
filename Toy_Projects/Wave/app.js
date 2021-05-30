aa68d7bed8d8e86d113d86cd64b2d172de15bb68
wjgin <woojin0913@naver.com>
2021-05-30 23:22:22 +0900

    this

--- Toy_Projects/Wave/app.js
+++ Toy_Projects/Wave/app.js
@@ -3,5 +3,24 @@ class App {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        requestAnimationFrame(this.animate.bind(this));
+    }
+
+    resize(){
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeihgt = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth *2;
+        this.canvas.height = this.stageHeihgt *2;
+        this.ctx.scale(2, 2);
+    }
+
+    animate(t) {
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeihgt);
+        requestAnimationFrame(this.animate.bind(this));
     }
 }
