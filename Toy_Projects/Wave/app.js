9c74e54e58a1d574f1a067f8d7e5e72617ea9c4e
wjgin <woojin0913@naver.com>
2021-05-31 21:54:22 +0900

    python_loaclserver

--- Toy_Projects/Wave/app.js
+++ Toy_Projects/Wave/app.js
@@ -1,8 +1,14 @@
+import {
+    Wave
+} from './wave.js';
+
 class App {
     constructor() {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
+
+        this.wave = new Wave();
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
@@ -17,10 +23,19 @@ class App {
         this.canvas.width = this.stageWidth *2;
         this.canvas.height = this.stageHeihgt *2;
         this.ctx.scale(2, 2);
+
+        this.wave.resize(this.stageWidth, this.stageHeihgt);
     }
 
     animate(t) {
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeihgt);
+        
+        this.wave.draw(this.ctx);
+        
         requestAnimationFrame(this.animate.bind(this));
     }
 }
+
+window.onload = () => {
+    new App();
+};
