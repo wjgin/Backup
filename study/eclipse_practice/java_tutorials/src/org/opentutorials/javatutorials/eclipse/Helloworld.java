4a3ac7dbc3c1d66d5adfa0d3cee0379aea7aba9e
wjgin <woojin0913@naver.com>
2021-07-18 21:55:12 +0900

    installed eclipse

---
+++ study/eclipse_practice/java_tutorials/src/org/opentutorials/javatutorials/eclipse/Helloworld.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.eclipse;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		String hello = "Hello";
+		String world = "world";
+		System.out.println(hello +' '+ world);
+		System.out.println(hello.length());
+	}
+
+}
