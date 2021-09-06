fa3b1ca3c318c91969c0de46fcc3d5f2a26368f2
wjgin <woojin0913@naver.com>
2021-09-06 17:49:41 +0900

    HTML_practice

---
+++ study/algorithm/Heap.java
@@ -0,0 +1,14 @@
+public class Heap {
+    public static void main(String[] args) {
+        int[] arr = {5, 7, 1, 8, 9, 4, 2, 3};
+
+        
+    }
+
+    public int[] Heapify(int[] arr) {
+        int len = arr.length;
+        
+
+        return arr;
+    }
+}
