c548174499dea72b0508fafc013c450a6ea428a4
wjgin <woojin0913@naver.com>
2021-09-06 11:46:16 +0900

    java_commit_byVSgit

--- study/eclipse_practice/java_tutorials/src/org/opentutorials/javatutorials/eclipse/Helloworld.java
+++ study/eclipse_practice/java_tutorials/src/org/opentutorials/javatutorials/eclipse/Helloworld.java
@@ -3,7 +3,6 @@ package org.opentutorials.javatutorials.eclipse;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		String hello = "Hello";
 		String world = "world";
