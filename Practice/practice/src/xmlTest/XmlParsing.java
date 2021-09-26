6832ee2d57614c2e1c02d88825b66b2490038cb0
wjgin <woojin0913@naver.com>
2021-09-26 22:19:40 +0900

    xml_parsing_getitems

--- Practice/practice/src/xmlTest/XmlParsing.java
+++ Practice/practice/src/xmlTest/XmlParsing.java
@@ -19,22 +19,30 @@ public class XmlParsing {
 		// xml 파싱 빌드업
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
+		
 		// xml 파일을 document로 파싱하기
 		Document document = builder.parse("xml/sample.xml");
-		// document 안의 노드의 리스트 가져오기
-		NodeList nodeList = document.getDocumentElement().getChildNodes();
-		// 첫 요소는 root 요소
+		
+		// root 요소 가져오기
 		Element root = document.getDocumentElement();
-		System.out.println(root);
-		//NodeList childList = root.getChildNodes();
-		NodeList name = root.getElementsByTagName("name");
-		System.out.println(name.item(0));
+		// root 요소 확인 : 첫 태그 sample
+		System.out.println(root.getNodeName()); 
+		// root 요소의 첫번째 노드는 #Text
+		Node firstNode = root.getFirstChild();
+		// 다음 노드는 customer
+		Node customer = firstNode.getNextSibling();
+		// customer 요소 안의 노드 리스트
+		NodeList childList = customer.getChildNodes();
 		
-//		for(int i = 0; i < nodeList.getLength(); i++) {
-//			System.out.println(childList.item(i).getNodeName());
-//			System.out.println(childList.item(i).getTextContent());
-//		}
-		
+		// customer 태그 하위 노드 리스트들의 내용을 출력
+		for(int i = 0; i < childList.getLength(); i++) {
+			Node item = childList.item(i);
+			if(item.getNodeType() == Node.ELEMENT_NODE) { // 노드의 타입이 Element일 경우(공백이 아닌 경우)
+				System.out.println(item.getNodeName());
+				System.out.println(item.getNodeValue());
+				System.out.println(item.getTextContent());
+			}
+		}
 	}
 
 }
