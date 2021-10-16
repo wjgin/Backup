f2e80c3f35177a8f3db64d90d71720b53b888e62
wjgin <woojin0913@naver.com>
2021-10-16 12:46:00 +0900

    로그인, 로그아웃 연동완료

--- Toy_Projects/BargainMarket/JSP5JO/src_backup/src/controller/FrontController.java
+++ Toy_Projects/BargainMarket/JSP5JO/src_backup/src/controller/FrontController.java
@@ -13,6 +13,7 @@ import controller.action.Action;
 import controller.action.ActionForward;
 import controller.action.IndexActioin;
 import controller.action.LoginMoveAction;
+import controller.action.LogoutAction;
 import controller.action.MyFieldAction;
 import controller.action.MypageAction;
 import controller.action.RegistAction;
@@ -23,56 +24,58 @@ import controller.action.SignInAction;
 /**
  * Servlet implementation class controller
  */
-@WebServlet("*.do") 
-public class FrontController extends HttpServlet {  
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public FrontController() {
-        super();
-    }
 
-    @Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public FrontController() {
+		super();
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		ActionForward forward = new ActionForward();
 		String spath = request.getServletPath();
-		
+
 		// url Action
-		if(spath.equals("/index.do")) {
+		if (spath.equals("/index.do")) {
 			Action action = new IndexActioin();
 			forward = action.execute(request, response);
-		} else if(spath.equals("/myField.do")) {
+		} else if (spath.equals("/myField.do")) {
 			Action action = new MyFieldAction();
 			forward = action.execute(request, response);
-		}  else if(spath.equals("/mypage.do")) {
+		} else if (spath.equals("/mypage.do")) {
 			Action action = new MypageAction();
 			forward = action.execute(request, response);
-		} else if(spath.equals("/search.do")) {
+		} else if (spath.equals("/search.do")) {
 			Action action = new SearchAction();
 			forward = action.execute(request, response);
-		} else if(spath.equals("/login.do")) {
+		} else if (spath.equals("/login.do")) {
 			Action action = new LoginMoveAction();
-			forward = action.execute(request, response);   
-		}else if(spath.equals("/signIn.do")) {
+			forward = action.execute(request, response);
+		} else if (spath.equals("/signIn.do")) {
 			Action action = new SignInAction();
 			forward = action.execute(request, response);
-		}else if(spath.equals("/regist.do")) {
+		} else if (spath.equals("/regist.do")) {
 			Action action = new RegistMoveAction();
 			forward = action.execute(request, response);
-		}else if(spath.equals("/registSave.do")) {
+		} else if (spath.equals("/registSave.do")) {
 			Action action = new RegistAction();
+			forward = action.execute(request, response);
+		} else if (spath.equals("/logout.do")) {
+			Action action = new LogoutAction();
 			forward = action.execute(request, response);
 		}
 
-		
 		// request 변경 여부
-		if(!forward.isRedirect()) { // 타입이 boolean이면 getter가 isXXX
+		if (!forward.isRedirect()) { // 타입이 boolean이면 getter가 isXXX
 			RequestDispatcher rd = request.getRequestDispatcher(forward.getUrl());
 			rd.forward(request, response);
 		} else {
 			response.sendRedirect(forward.getUrl());
-		} 
-		
-	}
-	
-}
+		}
 
+	}
+
+}
